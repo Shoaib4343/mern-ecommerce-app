8ba6956ef517f6334ccd001a80ca9096517db1b3
Shoaib <mohammadshoaibkhan4343@gmail.com>
2026-01-07 15:56:38 +0500

    feat(user): extend user schema with answer field

--- server/models/auth.model.js
+++ server/models/auth.model.js
@@ -34,6 +34,11 @@ const userSchema = new mongoose.Schema({
         trim: true,
         maxlength: 15,
     },
+    answer: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     role: {
         type: Number,
         default: 0, // 0 - user, 1 - admin
