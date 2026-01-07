66588d9c363edfde531ed34f98bba8657fadeab8
Shoaib <mohammadshoaibkhan4343@gmail.com>
2026-01-07 15:52:55 +0500

    add forgot password and user/admin auth routes

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,21 +1,18 @@
 import express from 'express';
-import { loginUserController, registerUserController } from '../controllers/auth.controller.js';
+import { forgotPasswordController, loginUserController, registerUserController } from '../controllers/auth.controller.js';
 import { validateMiddleware } from '../middlewares/joiValidate.middleware.js';
-import { loginSchema, registerSchema } from '../validator/auth.validator.js';
+import { forgotPasswordSchema, loginSchema, registerSchema } from '../validator/auth.validator.js';
 import { isAdmin, requireSignIn } from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
 
 // Define authentication routes here
 
-// Register route || METHOD POST
-router.post('/register',validateMiddleware(registerSchema), registerUserController);
-// Login route || METHOD POST
-router.post('/login',validateMiddleware(loginSchema), loginUserController);
-// test route
-router.get('/test', requireSignIn, isAdmin, (req, res) => {
-    res.send("TEST route is working...");
-}); 
 
-
+router.post('/register',validateMiddleware(registerSchema), registerUserController);                  // Register route || METHOD POST
+router.post('/login',validateMiddleware(loginSchema), loginUserController);                           // Login route || METHOD POST
+router.post("/forgot-password",validateMiddleware(forgotPasswordSchema), forgotPasswordController);   // Forget Password || METHOD POST
+router.get("/user-auth", requireSignIn, (req, res) => {res.status(200).json({success: true})});       // protected route user auth
+router.get("/admin-auth",requireSignIn,isAdmin,(req,res)=>{res.status(200).json({success:true});});   // protected route admin auth
+router.get('/test', requireSignIn, isAdmin, (req, res) => {res.send("TEST route is working...");});   // test route
 export default router;
