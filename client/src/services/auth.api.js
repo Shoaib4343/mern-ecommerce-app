38c9b2e645fcc52507d31909af8a4f9460faba5a
Shoaib <mohammadshoaibkhan4343@gmail.com>
2026-01-12 03:46:35 +0500

    refactor: simplify auth APIs by removing manual auth headers

--- client/src/services/auth.api.js
+++ client/src/services/auth.api.js
@@ -1,46 +1,82 @@
+// import API from "./api";
+
+
+// // Register User
+// export const RegisterUserApi = async(userData) =>{
+//     return await API.post('/register', userData);
+// }
+
+// // Login User
+// export const LoginUserApi = async(loginData) =>{
+//     return await API.post('/login', loginData);
+// }
+
+// // check user Auth
+// export const checkAuthApi = async()=>{
+//     const auth = JSON.parse(localStorage.getItem("auth"));
+//     if(!auth?.token){
+//         throw new Error("No auth token found");
+//     }
+//     return await API.get("/user-auth",{
+//         headers:{
+//             Authorization: `Bearer ${auth?.token}`
+//         }
+//     })
+// }
+
+
+// export const checkAdminAuthApi = async()=>{
+//     const auth = JSON.parse(localStorage.getItem("auth"));
+//     if(!auth?.token){
+//         throw new Error("No auth token found");
+//     }
+
+//     return await API.get("/admin-auth",{
+//         headers:{
+//             Authorization: `Bearer ${auth?.token}`
+//         }
+//     })
+// }
+
+
+
+// // Forgot Password
+// export const ForgotPasswordApi = async(formData)=>{
+//     return await API.post('/forgot-password', formData);
+// }
+
+
+
+
+
+
+
+
+
+
 import API from "./api";
 
+// Register
+export const RegisterUserApi = (userData) => {
+  return API.post("/register", userData);
+};
 
-// Register User
-export const RegisterUserApi = async(userData) =>{
-    return await API.post('/register', userData);
-}
+// Login
+export const LoginUserApi = (loginData) => {
+  return API.post("/login", loginData);
+};
 
-// Login User
-export const LoginUserApi = async(loginData) =>{
-    return await API.post('/login', loginData);
-}
+// Check User Auth
+export const checkAuthApi = () => {
+  return API.get("/user-auth");
+};
 
-// check user Auth
-export const checkAuthApi = async()=>{
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    if(!auth?.token){
-        throw new Error("No auth token found");
-    }
-    return await API.get("/user-auth",{
-        headers:{
-            Authorization: `Bearer ${auth?.token}`
-        }
-    })
-}
-
-
-export const checkAdminAuthApi = async()=>{
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    if(!auth?.token){
-        throw new Error("No auth token found");
-    }
-
-    return await API.get("/admin-auth",{
-        headers:{
-            Authorization: `Bearer ${auth?.token}`
-        }
-    })
-}
-
-
+// Check Admin Auth
+export const checkAdminAuthApi = () => {
+  return API.get("/admin-auth");
+};
 
 // Forgot Password
-export const ForgotPasswordApi = async(formData)=>{
-    return await API.post('/forgot-password', formData);
-}
+export const ForgotPasswordApi = (formData) => {
+  return API.post("/forgot-password", formData);
+};
