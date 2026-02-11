7d81bbaa75fc310b200cc5fb8346ac976069bb94
Shoaib <mohammadshoaibkhan4343@gmail.com>
2026-02-12 04:08:09 +0500

    feat: wrap App with AddToCartContextProvider for global cart state

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { AddToCartContextProvider } from "./context/AddToCartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
-  </AuthProvider>
+    <AddToCartContextProvider>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </AddToCartContextProvider>
+  </AuthProvider>,
 );
